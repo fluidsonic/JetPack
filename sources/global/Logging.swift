57cd4a5aca8e5f21de64e65ef8ecbe635876de54
Marc Knaup <marc.knaup@koeln.de>
2014-10-20 16:22:24 +0200

    Logging can be enabled/disabled at runtime now. Defaults to enabled when compiling in debug configuration.

--- sources/global/Logging.swift
+++ sources/global/Logging.swift
@@ -1,26 +1,40 @@
-public func LOG(_ message: (@autoclosure () -> String)? = nil, function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UWord = __LINE__) {
-	#if LOG
-		let fileName = file.stringValue.lastPathComponent.stringByDeletingPathExtension
+import Foundation
 
-		let evaluatedMessage = message?()
-		if let evaluatedMessage = evaluatedMessage {
-			NSLog("%@", "\(fileName)/\(function.stringValue):\(line) | \(evaluatedMessage)")
-		}
-		else {
-			NSLog("%@", "\(fileName)/\(function.stringValue):\(line)")
-		}
-	#endif
+#if DEBUG
+	public var LOGenabled = true
+#else
+	public var LOGenabled = false
+#endif
+
+
+public func LOG(_ message: (@autoclosure () -> String)? = nil, function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UWord = __LINE__) {
+	if !LOGenabled {
+		return
+	}
+
+	let fileName = file.stringValue.lastPathComponent.stringByDeletingPathExtension
+
+	let evaluatedMessage = message?()
+	if let evaluatedMessage = evaluatedMessage {
+		NSLog("%@", "\(fileName)/\(function.stringValue):\(line) | \(evaluatedMessage)")
+	}
+	else {
+		NSLog("%@", "\(fileName)/\(function.stringValue):\(line)")
+	}
 }
 
 
+// temporary workaround for lack of #warning instruction
 public func WARN(_ message: (@autoclosure () -> String)? = nil, function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UWord = __LINE__) {
-	#if LOG
-		let evaluatedMessage = message?()
-		if let evaluatedMessage = evaluatedMessage {
-			LOG("Warning: \(evaluatedMessage)", function: function, file: file, line: line)
-		}
-		else {
-			LOG("Warning!", function: function, file: file, line: line)
-		}
-	#endif
+	if !LOGenabled {
+		return
+	}
+
+	let evaluatedMessage = message?()
+	if let evaluatedMessage = evaluatedMessage {
+		LOG("Warning: \(evaluatedMessage)", function: function, file: file, line: line)
+	}
+	else {
+		LOG("Warning!", function: function, file: file, line: line)
+	}
 }
