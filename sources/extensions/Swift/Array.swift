14e9742bdf46ce43c7cb4d3e7fd5bb4fdfb817df
Marc Knaup <marc.knaup@koeln.de>
2014-10-24 19:19:14 +0200

    Removed explicit use of module name so sources can be added to the target project directly instead of using a dynamic framework.

--- sources/extensions/Swift/Array.swift
+++ sources/extensions/Swift/Array.swift
@@ -1,8 +1,14 @@
 // make public once the compiler allows it
 
+// until we no longer need MODULE.conflictedName (MODULE can vary depending on how you use this framework)
+private func _separate<E, S : SequenceType where S.Generator.Element == E>(source: S, isLeftElement: (E) -> Bool) -> ([E], [E]) {
+	return separate(source, isLeftElement)
+}
+
+
 extension Array {
 
 	func separate(isLeftElement: T -> Bool) -> ([T], [T]) {
-		return JetPack.separate(self, isLeftElement)
+		return _separate(self, isLeftElement)
 	}
 }
