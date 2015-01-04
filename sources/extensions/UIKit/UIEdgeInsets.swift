8e8237fd005f1c1158434158f484e569701e4fc0
Marc Knaup <marc.knaup@koeln.de>
2015-01-04 19:26:57 +0100

    Added == operator and empty constructor for UIEdgeInsets.

--- sources/extensions/UIKit/UIEdgeInsets.swift
+++ sources/extensions/UIKit/UIEdgeInsets.swift
@@ -3,6 +3,18 @@ import UIKit
 
 public extension UIEdgeInsets {
 
-	public static let zeroInsets = UIEdgeInsetsZero
+	public static let zeroInsets = UIEdgeInsets()
 
+
+	public init() {
+		self.init(top: 0, left: 0, bottom: 0, right: 0)
+	}
+}
+
+
+extension UIEdgeInsets: Equatable {}
+
+
+public func == (a: UIEdgeInsets, b: UIEdgeInsets) -> Bool {
+	return (a.top == b.top && a.left == b.left && a.bottom == b.bottom && a.right == b.right)
 }
