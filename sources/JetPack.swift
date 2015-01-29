bc84fac078bc8c5198d1dbdf2c8ba641e62499d7
Dennis Kiewning <dennis@widgetlabs.eu>
2015-01-29 15:42:35 +0100

    Workaround for Swift compiler bug in LOG.

--- sources/JetPack.swift
+++ sources/JetPack.swift
@@ -20,7 +20,7 @@ extension Array {
 
 	func separate(isLeftElement: T -> Bool) -> ([T], [T]) {
 		return JetPack.separate(self, isLeftElement)
-	}
+	} 
 }
 
 
--- sources/global/Logging.swift
+++ sources/global/Logging.swift
@@ -7,34 +7,21 @@ import Foundation
 #endif
 
 
-public func LOG(_ message: (@autoclosure () -> String)? = nil, function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UWord = __LINE__) {
+public func LOG(message: (@autoclosure () -> String), function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UWord = __LINE__) {
 	if !LOGenabled {
 		return
 	}
 
 	let fileName = file.stringValue.lastPathComponent.stringByDeletingPathExtension
-
-	let evaluatedMessage = message?()
-	if let evaluatedMessage = evaluatedMessage {
-		NSLog("%@", "\(fileName)/\(function.stringValue):\(line) | \(evaluatedMessage)")
-	}
-	else {
-		NSLog("%@", "\(fileName)/\(function.stringValue):\(line)")
-	}
+	NSLog("%@", "\(fileName)/\(function.stringValue):\(line) | \(message())")
 }
 
 
 // temporary workaround for lack of #warning instruction
-public func WARN(_ message: (@autoclosure () -> String)? = nil, function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UWord = __LINE__) {
+public func WARN(message: (@autoclosure () -> String), function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UWord = __LINE__) {
 	if !LOGenabled {
 		return
 	}
 
-	let evaluatedMessage = message?()
-	if let evaluatedMessage = evaluatedMessage {
-		LOG("Warning: \(evaluatedMessage)", function: function, file: file, line: line)
-	}
-	else {
-		LOG("Warning!", function: function, file: file, line: line)
-	}
+	LOG("Warning: \(message())", function: function, file: file, line: line)
 }
