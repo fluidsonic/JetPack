e394b9d79b0afb2a779b4db3112723d52c7f173d
Marc Knaup <marc.knaup@koeln.de>
2015-02-04 16:25:31 +0100

    Added extensions for NSOrderedSet: .isEmpty, .reversedSequenceOf(), .sequenceOf(), SequenceType

--- sources/extensions/Foundation/NSOrderedSet.swift
+++ sources/extensions/Foundation/NSOrderedSet.swift
@@ -6,4 +6,35 @@ public extension NSOrderedSet {
 	public convenience init<S: SequenceType where S.Generator.Element: AnyObject>(elements: S) {
 		self.init(array: Array(elements))
 	}
+
+
+	public var isEmpty: Bool {
+		return (count == 0)
+	}
+
+
+	public func reversedSequenceOf<T>(type: T.Type) -> SequenceOf<T> {
+		let generator = NSFastGenerator(reverseObjectEnumerator())
+
+		return SequenceOf(GeneratorOf() {
+			return generator.next() as T?
+		})
+	}
+
+
+	public func sequenceOf<T>(type: T.Type) -> SequenceOf<T> {
+		let generator = generate()
+
+		return SequenceOf(GeneratorOf() {
+			return generator.next() as T?
+		})
+	}
+}
+
+
+extension NSOrderedSet: SequenceType {
+
+	public func generate() -> NSFastGenerator {
+		return NSFastGenerator(self)
+	}
 }
