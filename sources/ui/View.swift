6707c41b8ce1a997229d2b2674a3a4ac5cda8534
Marc Knaup <marc.knaup@koeln.de>
2015-02-10 16:52:42 +0100

    Moved UIView.animate helpers to View to prevent collisions with ObjC methods at runtime.

--- sources/ui/View.swift
+++ sources/ui/View.swift
@@ -11,4 +11,90 @@ public class View: UIView {
 	public required init(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
+
+
+	public class func animate(#duration: NSTimeInterval, animations: () -> Void) {
+		animateWithDuration(duration, delay: 0, options: nil, animations: animations, completion: nil)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, animations: () -> Void, completion: Bool -> Void) {
+		animateWithDuration(duration, delay: 0, options: nil, animations: animations, completion: completion)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, options: UIViewAnimationOptions, animations: () -> Void) {
+		animateWithDuration(duration, delay: 0, options: options, animations: animations, completion: nil)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, options: UIViewAnimationOptions, animations: () -> Void, completion: Bool -> Void) {
+		animateWithDuration(duration, delay: 0, options: options, animations: animations, completion: completion)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, options: UIViewAnimationOptions, delay: NSTimeInterval, animations: () -> Void) {
+		animateWithDuration(duration, delay: delay, options: options, animations: animations, completion: nil)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, options: UIViewAnimationOptions, delay: NSTimeInterval, animations: () -> Void, completion: Bool -> Void) {
+		animateWithDuration(duration, delay: delay, options: options, animations: animations, completion: completion)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, animations: () -> Void) {
+		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: nil, animations: animations, completion: nil)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, animations: () -> Void, completion: Bool -> Void) {
+		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: nil, animations: animations, completion: completion)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, options: UIViewAnimationOptions, animations: () -> Void) {
+		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: options, animations: animations, completion: nil)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, options: UIViewAnimationOptions, animations: () -> Void, completion: Bool -> Void) {		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: options, animations: animations, completion: completion)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, options: UIViewAnimationOptions, delay: NSTimeInterval, animations: () -> Void) {
+		animateWithDuration(duration, delay: delay, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: options, animations: animations, completion: nil)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, options: UIViewAnimationOptions, delay: NSTimeInterval, animations: () -> Void, completion: Bool -> Void) {
+		animateWithDuration(duration, delay: delay, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: options, animations: animations, completion: completion)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, animations: () -> Void) {
+		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: nil, animations: animations, completion: nil)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, animations: () -> Void, completion: Bool -> Void) {
+		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: nil, animations: animations, completion: completion)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, options: UIViewAnimationOptions, animations: () -> Void) {		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: options, animations: animations, completion: nil)
+	}
+
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, options: UIViewAnimationOptions, animations: () -> Void, completion: Bool -> Void) {
+		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: options, animations: animations, completion: completion)
+	}
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, options: UIViewAnimationOptions, delay: NSTimeInterval, animations: () -> Void) {
+		animateWithDuration(duration, delay: delay, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: options, animations: animations, completion: nil)
+	}
+
+	public class func animate(#duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, options: UIViewAnimationOptions, delay: NSTimeInterval, animations: () -> Void, completion: Bool -> Void) {
+		animateWithDuration(duration, delay: delay, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: options, animations: animations, completion: completion)
+	}
 }
