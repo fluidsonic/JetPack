9cd35edfb368a7eb9b9976dad4f513c50ddb24e9
Marc Knaup <marc.knaup@koeln.de>
2015-03-20 19:06:54 +0100

    Added NSDate.isSameDayAs

--- sources/extensions/Foundation/NSDate.swift
+++ sources/extensions/Foundation/NSDate.swift
@@ -1,6 +1,21 @@
 import Foundation
 
 
+public extension NSDate {
+
+	public func isSameDayAs(otherDate: NSDate) -> Bool {
+		let calendar = NSCalendar.currentCalendar()
+		let components = calendar.components(.CalendarUnitDay | .CalendarUnitMonth | .CalendarUnitYear | .CalendarUnitEra, fromDate: self)
+		let otherComponents = calendar.components(.CalendarUnitDay | .CalendarUnitMonth | .CalendarUnitYear | .CalendarUnitEra, fromDate: otherDate)
+
+		return (components.day == otherComponents.day
+			&& components.month == otherComponents.month
+			&& components.year == otherComponents.year
+			&& components.era == otherComponents.era)
+	}
+}
+
+
 extension NSDate: Comparable {}
 
 public func < (a: NSDate, b: NSDate) -> Bool {
