2ca8e94800b8d96d4bc8e84208a7e921f63a3554
Marc Knaup <marc.knaup@koeln.de>
2015-03-24 15:14:11 +0100

    Added CGPoint(left:, top:)

--- sources/extensions/CoreGraphics/CGPoint.swift
+++ sources/extensions/CoreGraphics/CGPoint.swift
@@ -3,26 +3,36 @@ import CoreGraphics
 
 public extension CGPoint {
 
+	public init(left: CGFloat, top: CGFloat) {
+		self.init(x: left, y: top)
+	}
+
+
 	public var left: CGFloat {
 		get { return x }
 		set { x = newValue }
 	}
 
+
 	public func pointByOffsetting(offset: CGPoint) -> CGPoint {
 		return CGPoint(x: x + offset.x, y: y + offset.y)
 	}
+
 
 	public func pointByOffsetting(#dx: CGFloat, dy: CGFloat) -> CGPoint {
 		return CGPoint(x: x + dx, y: dy)
 	}
 
+
 	public func pointByOffsetting(#dx: CGFloat) -> CGPoint {
 		return CGPoint(x: x + dx, y: y)
 	}
 
+
 	public func pointByOffsetting(#dy: CGFloat) -> CGPoint {
 		return CGPoint(x: x, y: y + dy)
 	}
+
 
 	public var top: CGFloat {
 		get { return y }
