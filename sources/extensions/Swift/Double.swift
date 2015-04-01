3c8f4e60999b9788927af6f5de231cb0f9041704
Marc Knaup <marc.knaup@koeln.de>
2015-04-01 21:08:16 +0200

    Added Double .clamped

--- sources/extensions/Swift/Double.swift
+++ sources/extensions/Swift/Double.swift
@@ -1,5 +1,10 @@
 public extension Double {
 
+	public func clamped(#min: Double, max: Double) -> Double {
+		return Swift.min(Swift.max(self, min), max)
+	}
+
+
 	public static var Epsilon: Double {
 		return 1.19209290e-07
 	}
