fb8247613a43ee8b5425f21b0954c58ef4e25754
Marc Knaup <marc.knaup@koeln.de>
2015-04-10 16:18:45 +0200

    Added NSMutableAttributedString extension.

---
+++ sources/extensions/Foundation/NSMutableAttributedString.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+
+public extension NSMutableAttributedString {
+
+	public func appendString(string: String, attribute: String, value: AnyObject) {
+		let location = length
+
+		replaceCharactersInRange(NSRange(location: location, length: 0), withString: string)
+		addAttribute(attribute, value: value, range: NSRange(location: location, length: length - location))
+	}
+
+
+	public func appendString(string: String, color: UIColor) {
+		appendString(string, attribute: NSForegroundColorAttributeName, value: color)
+	}
+
+
+	public func appendString(string: String, attributes: [NSObject : AnyObject]) {
+		let location = length
+
+		replaceCharactersInRange(NSRange(location: location, length: 0), withString: string)
+
+		if !attributes.isEmpty {
+			addAttributes(attributes, range: NSRange(location: location, length: length - location))
+		}
+	}
+}
