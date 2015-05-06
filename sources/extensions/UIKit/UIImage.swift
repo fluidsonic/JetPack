8c703bb6dcf936b2fc65786084e07ba370493ae9
Dennis Kiewning <dennis@widgetlabs.eu>
2015-05-06 17:40:37 +0200

    Added UIImage(placeholderOfSize:)

--- sources/extensions/UIKit/UIImage.swift
+++ sources/extensions/UIKit/UIImage.swift
@@ -3,6 +3,14 @@ import UIKit
 
 public extension UIImage {
 
+	public convenience init?(placeholderOfSize size: CGSize) {
+		let context = CGBitmapContextCreate(nil, Int(ceil(size.width)), Int(ceil(size.height)), 8, 0, CGColorSpaceCreateDeviceRGB(), CGBitmapInfo(rawValue: CGImageAlphaInfo.PremultipliedLast.rawValue))
+		let cgimage = CGBitmapContextCreateImage(context)
+
+		self.init(CGImage: cgimage, scale: 1, orientation: .Up)
+	}
+
+
 	public var hasAlphaChannel: Bool {
 		let info = CGImageGetAlphaInfo(CGImage)
 		switch info {
