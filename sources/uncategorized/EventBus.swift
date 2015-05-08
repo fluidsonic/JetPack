143c154513b8fb4da8d45e3a1f6eafb76b64d19e
Marc Knaup <marc@knaup.koeln>
2015-05-08 13:11:39 +0200

    Added an EventBus, yay\!

---
+++ sources/uncategorized/EventBus.swift
@@ -0,0 +1,117 @@
+public class EventBus {
+
+	private let lock = EmptyObject()
+	private var subscriptionsByScope = [ObjectIdentifier : AnySubscriptions]()
+
+
+
+	public init() {}
+
+
+	public func publish<T>(object: T) {
+		synchronized(lock) {
+			let scope = ObjectIdentifier(T.Type)
+
+			if let subscriptions = subscriptionsByScope[scope] as! Subscriptions<T>? {
+				for subscription in subscriptions.list {
+					subscription.callback(object)
+				}
+			}
+		}
+	}
+
+
+	public func pool() -> Pool {
+		return Pool(eventBus: self)
+	}
+
+
+	public func subscribe<T>(callback: (T) -> Void) -> Block {
+		return synchronized(lock) {
+			let scope = ObjectIdentifier(T.Type)
+			let subscription = Subscription(callback: callback)
+
+			if let subscriptions = subscriptionsByScope[scope] as! Subscriptions<T>? {
+				subscriptions.list.append(subscription)
+			}
+			else {
+				let subscriptions = Subscriptions<T>()
+				subscriptions.list.append(subscription)
+				subscriptionsByScope[scope] = subscriptions
+			}
+
+			var unsubscribeToken: dispatch_once_t = 0
+
+			return {
+				dispatch_once(&unsubscribeToken) {
+					if let subscriptions = self.subscriptionsByScope[scope] as! Subscriptions<T>? {
+						removeFirstIdentical(&subscriptions.list, subscription)
+
+						if subscriptions.list.isEmpty {
+							self.subscriptionsByScope[scope] = nil
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+
+	public final class Pool {
+
+		private let eventBus: EventBus
+		private let lock = EmptyObject()
+		private var unsubscribes = [Block]()
+
+
+		private init(eventBus: EventBus) {
+			self.eventBus = eventBus
+		}
+
+
+		deinit {
+			unsubscribeAll()
+		}
+
+
+		public func subscribe<T>(callback: (T) -> Void) {
+			synchronized(lock) {
+				unsubscribes.append(eventBus.subscribe(callback))
+			}
+		}
+
+
+		public func unsubscribeAll() {
+			synchronized(lock) {
+				for unsubscribe in unsubscribes {
+					unsubscribe()
+				}
+
+				unsubscribes.removeAll(keepCapacity: false)
+			}
+		}
+	}
+}
+
+
+
+private class Subscription<T> {
+
+	private let callback: (T) -> Void
+
+
+	private init(callback: (T) -> Void) {
+		self.callback = callback
+	}
+}
+
+
+
+private protocol AnySubscriptions {}
+
+private class Subscriptions<T>: AnySubscriptions {
+
+	private var list = [Subscription<T>]()
+
+}
