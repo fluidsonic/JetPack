eafbcbf3c371076958323c46320ebf641f1bc0dc
Marc Knaup <marc@knaup.koeln>
2015-05-08 13:11:20 +0200

    Replaced usage of AnyType with ObjectIdentifier & added ObjectGraph.get(T.Type)

--- sources/uncategorized/AnyType.swift
+++
@@ -1,1 +0,0 @@
-// AnyType temporarily moved to ObjectGraph.swift to prevent compiler crash in Swift 1.2 (Xcode 6.3 beta 1)
--- sources/uncategorized/ObjectGraph.swift
+++ sources/uncategorized/ObjectGraph.swift
@@ -1,75 +1,30 @@
-// AnyType temporarily moved to ObjectGraph.swift to prevent compiler crash in Swift 1.2 (Xcode 6.3 beta 1)
-// Swift doesn't give us the name of Any.Type yet so we have to rely on NSStringFromClass :(
-
-public struct AnyType {
-
-	public let hashValue: Int
-	public let name: String
-	public let type: AnyClass
-
-
-	public init(_ type: AnyClass) {
-		self.name = NSStringFromClass(type)
-		self.type = type
-
-		hashValue = name.hashValue
-	}
-
-
-	public var demangledName: String {
-		return toString(type)
-	}
-}
-
-
-extension AnyType: DebugPrintable {
-
-	public var debugDescription: String {
-		return name
-	}
-}
-
-
-extension AnyType: Hashable {}
-
-
-extension AnyType: Printable {
-
-	public var description: String {
-		return name
-	}
-}
-
-
-public func == (a: AnyType, b: AnyType) -> Bool {
-	return a.name == b.name
-}
-
-
-
-
 public class ObjectGraph {
 
-	private var objects = [AnyType : AnyObject]()
+	private var objects = [ObjectIdentifier : AnyObject]()
 
 
 	public init() {}
 
 
 	public func add<T: AnyObject>(object: T) {
-		objects[AnyType(T.self)] = object
+		objects[ObjectIdentifier(T.Type)] = object
 	}
 
 
 	public func get<T: AnyObject>() -> T {
-		let typeWrapper = AnyType(T.self)
-		if let object: AnyObject = objects[typeWrapper] {
-			return object as! T
+		let id = ObjectIdentifier(T.Type)
+		if let object = objects[id] as! T? {
+			return object
 		}
 
-		LOG("Cannot resolve graph object for type \(typeWrapper).")
+		LOG("Cannot resolve graph object for type \(T.self).")
 
 		preconditionFailure()
+	}
+
+
+	public func get<T: AnyObject>(type: T.Type) -> T {
+		return get()
 	}
 }
 
