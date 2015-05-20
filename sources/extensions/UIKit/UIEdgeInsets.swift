05577edd2b90d56720d2db424b7ca959e1228947
Marc Knaup <marc@knaup.koeln>
2015-05-20 17:16:40 +0200

    Added UIEdgeInsets(all:)

--- sources/extensions/UIKit/UIEdgeInsets.swift
+++ sources/extensions/UIKit/UIEdgeInsets.swift
@@ -4,6 +4,11 @@ import UIKit
 public extension UIEdgeInsets {
 
 	public static let zeroInsets = UIEdgeInsets()
+
+
+	public init(all: CGFloat) {
+		self.init(top: all, left: all, bottom: all, right: all)
+	}
 }
 
 
