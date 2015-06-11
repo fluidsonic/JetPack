b2a76b87b3bb58338ea0a9d9b6b1881b96bafcb7
Marc Knaup <marc@knaup.koeln>
2015-06-11 18:57:11 +0200

    Added optional nameSuffix parameter to File.createTemporary()

--- sources/io/File.swift
+++ sources/io/File.swift
@@ -6,11 +6,19 @@ public final class File {
 	private init() {} // static class for now
 
 
-	public static func createTemporary(namePrefix: String = "") -> NSURL? {
-		let pathTemplate = NSTemporaryDirectory().stringByAppendingPathComponent("\(namePrefix)XXXXXXXXXX")
+	public static func createTemporary(namePrefix: String = "", nameSuffix: String = "") -> NSURL? {
+		let pathTemplate = NSTemporaryDirectory().stringByAppendingPathComponent("\(namePrefix)XXXXXXXXXX\(nameSuffix)")
 		var cPath = pathTemplate.fileSystemRepresentation()
 
-		let fileDescriptor = mkstemp(&cPath)
+		let fileDescriptor: Int32
+
+		if nameSuffix.isEmpty {
+			fileDescriptor = mkstemp(&cPath)
+		}
+		else {
+			fileDescriptor = mkstemps(&cPath, Int32(nameSuffix.fileSystemRepresentation().count))
+		}
+
 		if fileDescriptor == -1 {
 			return nil
 		}
