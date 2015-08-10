3bd592dd78ef099c9f5a24bf554620dd126c685c
bwerker <benjamin@widgetlabs.eu>
2015-08-10 11:39:55 +0200

    replaced UWord with Uint as mentioned in Xcode 7 beta 5 changelogs refactored class File to use the UnsafeMutablePointer as of Xcode 7 beta 5 UnsafePointer was not usable anymore

--- sources/global/Logging.swift
+++ sources/global/Logging.swift
@@ -7,18 +7,18 @@ import Foundation
 #endif
 
 
-public func LOG(@autoclosure message:() -> String, function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UWord = __LINE__) {
+public func LOG(@autoclosure message: () -> String, function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UInt = __LINE__) {
 	if !LOGenabled {
 		return
 	}
 
-	let fileName = file.stringValue.lastPathComponent.stringByDeletingPathExtension
+	let fileName = ((file.stringValue as NSString).lastPathComponent as NSString).stringByDeletingPathExtension
 	NSLog("%@", "\(fileName)/\(function.stringValue):\(line) | \(message())")
 }
 
 
 // temporary workaround for lack of #warning instruction
-public func WARN(@autoclosure message:() -> String, function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UWord = __LINE__) {
+public func WARN(@autoclosure message: () -> String, function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UInt = __LINE__) {
 	if !LOGenabled {
 		return
 	}
