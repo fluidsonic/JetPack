4eccdd416d4e3a550ef2826d70054ddda55e8e72
Marc Knaup <marc@knaup.koeln>
2015-08-14 13:47:04 +0200

    Added UIEdgeInsets.insetRect() and .isEmpty

--- sources/extensions/UIKit/UIEdgeInsets.swift
+++ sources/extensions/UIKit/UIEdgeInsets.swift
@@ -9,6 +9,21 @@ public extension UIEdgeInsets {
 	public init(all: CGFloat) {
 		self.init(top: all, left: all, bottom: all, right: all)
 	}
+
+
+	public func insetRect(rect: CGRect) -> CGRect {
+		return CGRect(
+			left:   rect.left + left,
+			top:    rect.top + top,
+			width:  rect.width - left - right,
+			height: rect.height - top - bottom
+		).standardizedRect
+	}
+
+
+	public var isEmpty: Bool {
+		return (top == 0 && left == 0 && bottom == 0 && right == 0)
+	}
 }
 
 
