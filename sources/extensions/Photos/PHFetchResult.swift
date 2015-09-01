f6e94a7d54368fffe6a7c03fe68f213cdf0faa74
Marc Knaup <marc@knaup.koeln>
2015-09-01 16:42:05 +0200

    Removed SequenceType-conformance for PHFetchResult due to Swift bug

--- sources/extensions/Photos/PHFetchResult.swift
+++ sources/extensions/Photos/PHFetchResult.swift
@@ -1,3 +1,7 @@
+// TODO: Declaring protocol conformance for PHFetchResult which is not available on iOS 7 will cause runtime crashes on iOS 7,
+//       no matter if the class is used or not. Add this again once iOS 7 support was dropped.
+
+/*
 import Photos
 
 
@@ -7,3 +11,4 @@ extension PHFetchResult: SequenceType {
 		return NSFastGenerator(self)
 	}
 }
+*/
