a3873a7b39e1ab8d1fdc681e4e52878a28891ef7
Marc Knaup <marc@knaup.koeln>
2015-09-08 18:52:46 +0200

    Improved TypeInstanceDictionary to support Optional and non-Optional return types.

--- sources/uncategorized/TypeInstanceDictionary.swift
+++ sources/uncategorized/TypeInstanceDictionary.swift
@@ -20,27 +20,50 @@ public struct TypeInstanceDictionary {
 
 		instances[id] = instance
 	}
-	
+
 
 	// Be very careful to not call this method with an Optional or an ImplicitlyUnwrappedOptional type. Use `as` in this case to expect the returned instance to be a non-optional.
-	public func get<T>() -> T {
+	public func get<T>() -> T? {
 		return get(T.self)
 	}
 
 
-	public func get<T>(type: T.Type) -> T {
+	public func get<T>(type: T.Type) -> T? {
 		let id = ObjectIdentifier(type)
-		if let instance = instances[id] as! T? {
-			return instance
+		return instances[id] as! T?
+	}
+
+
+	// Be very careful to not call this method with an Optional or an ImplicitlyUnwrappedOptional type. Use `as` in this case to expect the returned instance to be a non-optional.
+	public func require<T>() -> T {
+		return require(T.self)
+	}
+
+
+	public func require<T>(type: T.Type) -> T {
+		guard let instance = get(type) else {
+			fatalError("No instance was assigned to type \(type).")
 		}
 
-		fatalError("No instance was assigned to type \(type).")
+		return instance
+	}
+
+
+	// Be very careful to not call this method with an Optional or an ImplicitlyUnwrappedOptional type. Use `as` in this case to convert the instance type to a non-optional first.
+	public mutating func unassign<T>(type: T.Type) -> T? {
+		let id = ObjectIdentifier(type)
+		return instances.removeValueForKey(id) as! T?
 	}
 }
 
 
-prefix operator << {}
+prefix operator <? {}
+prefix operator <! {}
 
-public prefix func << <T>(dictionary: TypeInstanceDictionary) -> T {
+public prefix func <? <T>(dictionary: TypeInstanceDictionary) -> T? {
 	return dictionary.get()
+}
+
+public prefix func <! <T>(dictionary: TypeInstanceDictionary) -> T {
+	return dictionary.require()
 }
