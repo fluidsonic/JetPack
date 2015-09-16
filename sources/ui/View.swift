b2a7cfbe06b13f58e8730918e2ae5c5590b3797c
Benjamin Werker <benjamin@widgetlabs.eu>
2015-09-16 14:28:59 +0200

    added backgroundColorLocked and added variables to Interface Builder

--- sources/ui/View.swift
+++ sources/ui/View.swift
@@ -2,116 +2,132 @@ import UIKit
 
 
 public class View: UIView {
-
-	public var userInteractionLimitedToSubviews = false
-
-
+	
+	@IBInspectable
+	public var backgroundColorLocked: Bool = false
+	
+	@IBInspectable
+	public var userInteractionLimitedToSubviews: Bool = false
+	
+	
 	public init() {
 		super.init(frame: .zero)
 	}
-
-
+	
+	
 	public required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
+		super.init(coder: coder)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, changes: () -> Void) {
 		animateWithDuration(duration, delay: 0, options: [], animations: changes, completion: nil)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, changes: () -> Void, completion: Bool -> Void) {
 		animateWithDuration(duration, delay: 0, options: [], animations: changes, completion: completion)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, options: UIViewAnimationOptions, changes: () -> Void) {
 		animateWithDuration(duration, delay: 0, options: options, animations: changes, completion: nil)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, options: UIViewAnimationOptions, changes: () -> Void, completion: Bool -> Void) {
 		animateWithDuration(duration, delay: 0, options: options, animations: changes, completion: completion)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, options: UIViewAnimationOptions, delay: NSTimeInterval, changes: () -> Void) {
 		animateWithDuration(duration, delay: delay, options: options, animations: changes, completion: nil)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, options: UIViewAnimationOptions, delay: NSTimeInterval, changes: () -> Void, completion: Bool -> Void) {
 		animateWithDuration(duration, delay: delay, options: options, animations: changes, completion: completion)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, changes: () -> Void) {
 		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: [], animations: changes, completion: nil)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, changes: () -> Void, completion: Bool -> Void) {
 		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: [], animations: changes, completion: completion)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, options: UIViewAnimationOptions, changes: () -> Void) {
 		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: options, animations: changes, completion: nil)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, options: UIViewAnimationOptions, changes: () -> Void, completion: Bool -> Void) {
 		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: options, animations: changes, completion: completion)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, options: UIViewAnimationOptions, delay: NSTimeInterval, changes: () -> Void) {
 		animateWithDuration(duration, delay: delay, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: options, animations: changes, completion: nil)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, options: UIViewAnimationOptions, delay: NSTimeInterval, changes: () -> Void, completion: Bool -> Void) {
 		animateWithDuration(duration, delay: delay, usingSpringWithDamping: damping, initialSpringVelocity: 0, options: options, animations: changes, completion: completion)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, changes: () -> Void) {
 		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: [], animations: changes, completion: nil)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, changes: () -> Void, completion: Bool -> Void) {
 		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: [], animations: changes, completion: completion)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, options: UIViewAnimationOptions, changes: () -> Void) {
 		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: options, animations: changes, completion: nil)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, options: UIViewAnimationOptions, changes: () -> Void, completion: Bool -> Void) {
 		animateWithDuration(duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: options, animations: changes, completion: completion)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, options: UIViewAnimationOptions, delay: NSTimeInterval, changes: () -> Void) {
 		animateWithDuration(duration, delay: delay, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: options, animations: changes, completion: nil)
 	}
-
-
+	
+	
 	public class func animate(duration duration: NSTimeInterval, usingSpringWithDamping damping: CGFloat, initialSpringVelocity: CGFloat, options: UIViewAnimationOptions, delay: NSTimeInterval, changes: () -> Void, completion: Bool -> Void) {
 		animateWithDuration(duration, delay: delay, usingSpringWithDamping: damping, initialSpringVelocity: initialSpringVelocity, options: options, animations: changes, completion: completion)
 	}
-
-
+	
+	
+	public override var backgroundColor: UIColor? {
+		get { return super.backgroundColor }
+		set {
+			if backgroundColorLocked {
+				return
+			}
+			
+			super.backgroundColor = newValue
+		}
+	}
+	
+	
 	public override func hitTest(point: CGPoint, withEvent event: UIEvent?) -> UIView? {
 		let view = super.hitTest(point, withEvent: event)
 		if userInteractionLimitedToSubviews && view === self {
 			return nil
 		}
-
+		
 		return view
 	}
 }
