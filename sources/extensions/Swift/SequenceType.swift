08e2bac7f33f77f047dbdc3a641316504ec19f31
Marc Knaup <marc@knaup.koeln>
2015-09-29 15:48:03 +0200

    Added SequenceType.count() with predicate

--- sources/extensions/Swift/SequenceType.swift
+++ sources/extensions/Swift/SequenceType.swift
@@ -1,3 +1,16 @@
+public extension SequenceType {
+
+	public func count(@noescape predicate: Generator.Element throws -> Bool) rethrows -> Int {
+		var count = 0
+		for element in self where try predicate(element) {
+			++count
+		}
+
+		return count
+	}
+}
+
+
 public extension SequenceType where Self.Generator.Element: AnyObject {
 
 	public func containsIdentical(element: Generator.Element) -> Bool {
