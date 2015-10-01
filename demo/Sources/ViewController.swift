2e79c5722312c07bd316ae918bdf80578e84796d
Marc Knaup <marc@knaup.koeln>
2015-10-01 21:19:40 +0200

    Added Demo project to fiddle around.

---
+++ demo/Sources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JetPack Demo
+//
+//  Created by Marc Knaup on 01/10/15.
+//  Copyright © 2015 Marc Knaup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
