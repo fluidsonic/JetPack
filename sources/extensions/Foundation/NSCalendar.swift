08d049f0c031207c314b7640db373c4a20070912
Marc Knaup <marc@knaup.koeln>
2015-10-19 13:24:59 +0200

    Renamed NSCalendar.dateBy(Adding|Subtracting)Seconds" since the first parameter is optional

--- sources/extensions/Foundation/NSCalendar.swift
+++ sources/extensions/Foundation/NSCalendar.swift
@@ -5,7 +5,7 @@ public extension NSCalendar {
 
 	@nonobjc
 	@warn_unused_result
-	public func dateByAddingSeconds(seconds: Int? = nil, minutes: Int? = nil, hours: Int? = nil, days: Int? = nil, months: Int? = nil, years: Int? = nil, toDate date: NSDate) -> NSDate? {
+	public func dateByAdding(seconds seconds: Int? = nil, minutes: Int? = nil, hours: Int? = nil, days: Int? = nil, months: Int? = nil, years: Int? = nil, toDate date: NSDate) -> NSDate? {
 		let components = NSDateComponents()
 
 		if let years = years {
@@ -33,7 +33,7 @@ public extension NSCalendar {
 
 	@nonobjc
 	@warn_unused_result
-	public func dateBySubtractingSeconds(seconds: Int? = nil, minutes: Int? = nil, hours: Int? = nil, days: Int? = nil, months: Int? = nil, years: Int? = nil, fromDate date: NSDate) -> NSDate? {
-		return dateByAddingSeconds(seconds.map(-), minutes: minutes.map(-), hours: hours.map(-), days: days.map(-), months: months.map(-), years: years.map(-), toDate: date)
+	public func dateBySubtracting(seconds seconds: Int? = nil, minutes: Int? = nil, hours: Int? = nil, days: Int? = nil, months: Int? = nil, years: Int? = nil, fromDate date: NSDate) -> NSDate? {
+		return dateByAdding(seconds: seconds.map(-), minutes: minutes.map(-), hours: hours.map(-), days: days.map(-), months: months.map(-), years: years.map(-), toDate: date)
 	}
 }
