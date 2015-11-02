b6a4f9594fa83004f0855be1458d360cba97e094
Marc Knaup <marc@knaup.koeln>
2015-11-02 14:24:50 +0100

    Added UICollectionView.firstResponderCell

---
+++ Sources/extensions/UIKit/UICollectionView.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+public extension UICollectionView {
+
+	@nonobjc
+	public var firstResponderCell: UICollectionViewCell? {
+		guard let firstResponder = firstResponder as? UIView else {
+			return nil
+		}
+
+		var optionalView: UIView? = firstResponder
+		while let view = optionalView {
+			guard view.superview === self else {
+				optionalView = view.superview
+				continue
+			}
+
+			return view as? UICollectionViewCell
+		}
+
+		return nil
+	}
+}
