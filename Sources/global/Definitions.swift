713c923af98eaeae2ab681ce66c98fbc63f09a0c
Marc Knaup <marc@knaup.koeln>
2015-11-02 12:36:45 +0100

    Removed Block and CancelBlock types which were marked as unavailable

--- Sources/global/Definitions.swift
+++ Sources/global/Definitions.swift
@@ -1,8 +1,2 @@
 public typealias Closure = () -> ()
 public typealias CancelClosure = Closure!  // temporary workaround for cancellation via closure until we have a nice struct instead
-
-@available(*, unavailable, renamed="Closure")
-public typealias Block = Closure
-
-@available(*, unavailable, renamed="CancelClosure")
-public typealias CancelBlock = CancelClosure
