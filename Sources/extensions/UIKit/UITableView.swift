4b7ce422c676208ceb4e1480db160be1a31c6823
Marc Knaup <marc@knaup.koeln>
2015-11-11 16:05:58 +0100

    UITableView.scrollRowAtIndexPathToVisible(_:insets:animated:) now returns true or false depening on whether a scroll will happen

--- Sources/extensions/UIKit/UITableView.swift
+++ Sources/extensions/UIKit/UITableView.swift
@@ -4,14 +4,19 @@ import UIKit
 public extension UITableView {
 
 	@nonobjc
-	public func scrollRowAtIndexPathToVisible(indexPath: NSIndexPath, insets: UIEdgeInsets = .zero, animated: Bool = false) {
+	public func scrollRowAtIndexPathToVisible(indexPath: NSIndexPath, insets: UIEdgeInsets = .zero, animated: Bool = false) -> Bool {
 		let contentSize = self.contentSize
 
 		var rect = rectForRowAtIndexPath(indexPath).insetBy(insets.inverse)
 		rect.left = rect.left.clamp(min: 0, max: max(contentSize.width - rect.width, 0))
 		rect.top = rect.top.clamp(min: 0, max: max(contentSize.height - rect.height, 0))
 
+		guard !bounds.insetBy(contentInset).contains(rect) else {
+			return false
+		}
+
 		scrollRectToVisible(rect, animated: animated)
+		return true
 	}
 
 
