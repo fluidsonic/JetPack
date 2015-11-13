29b865805773b3df531ccb863b4a9bb9bddfd04b
Marc Knaup <marc@knaup.koeln>
2015-11-13 18:27:33 +0100

    Removed stray log statement in TableViewController

--- Sources/ui/TableViewController.swift
+++ Sources/ui/TableViewController.swift
@@ -27,7 +27,6 @@ public /* non-final */ class TableViewController: ViewController {
 	public override func decorationInsetsDidChangeWithAnimation(animation: Animation?) {
 		super.decorationInsetsDidChangeWithAnimation(animation)
 
-		log("\(innerDecorationInsets)")
 		if automaticallyAdjustsTableViewInsets {
 			Animation.run(animation) {
 				tableView.setContentInset(innerDecorationInsets, maintainingVisualContentOffset: true)
