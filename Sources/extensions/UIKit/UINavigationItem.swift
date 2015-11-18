21af534f78dfe4ca88d462c110554a0f68b4d9e4
Marc Knaup <marc@knaup.koeln>
2015-11-18 18:54:29 +0100

    Added UINavigationItem extension

---
+++ Sources/extensions/UIKit/UINavigationItem.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+public extension UINavigationItem {
+
+	@nonobjc
+	public var backButtonTitles: Set<String> {
+		get { return backBarButtonItem?.possibleTitles ?? [] }
+		set {
+			guard newValue.isEmpty || newValue != backButtonTitles else {
+				return
+			}
+
+			if let anyTitle = newValue.first {
+				let item = UIBarButtonItem(title: anyTitle)
+				item.possibleTitles = newValue
+				backBarButtonItem = item
+			}
+			else {
+				backBarButtonItem = nil
+			}
+		}
+	}
+}
