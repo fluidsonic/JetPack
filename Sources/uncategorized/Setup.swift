84a4b81e07ea89e5546ebc7b79247a814abedc2d
Marc Knaup <marc@knaup.koeln>
2015-11-18 12:30:32 +0100

    Added TODO about unsafe static initialization (Setup)

--- Sources/uncategorized/Setup.swift
+++ Sources/uncategorized/Setup.swift
@@ -7,6 +7,8 @@ public extension UIApplication {
 	private static var onceToken = dispatch_once_t()
 
 
+	// TODO We should no longer rely on +initialize in extensions since it replaces UIKit's own implementation (if present)
+	//      and can also collide with other frameworks & libraries implementing +initialize on the same class.
 	@objc
 	public override class func initialize() {
 		dispatch_once(&onceToken) {
