6a5928f9e37c5643de592240eb9dbdbbfced862b
Marc Knaup <marc@Marc.local>
2015-11-21 18:57:02 +0100

    Added NSFileManager.asyncRemoveItem

---
+++ Sources/extensions/Foundation/NSFileManager.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+
+public extension NSFileManager {
+
+	@nonobjc
+	public func asyncRemoveItem(item: NSURL, handler: (Result<Void> -> Void)? = nil) {
+		asyncWithHandler(handler, errorMessage: "Cannot remove item '\(item)'") {
+			return try self.removeItemAtURL(item)
+		}
+	}
+
+
+	@nonobjc
+	private func asyncWithHandler <ValueType> (handler: (Result<ValueType> -> Void)?, @autoclosure(escaping) errorMessage: Void -> String, action: Void throws -> ValueType) {
+		onBackgroundQueueOfPriority(.Low) {
+			do {
+				handler?(.Success(try action()))
+			}
+			catch let error {
+				return self.reportError(error, withMessage: errorMessage(), toHandler: handler)
+			}
+		}
+	}
+
+
+	@nonobjc
+	private func reportError <ValueType> (error: ErrorType, @autoclosure withMessage message: Void -> String, function: StaticString = __FUNCTION__, file: StaticString = __FILE__, line: UInt = __LINE__, toHandler handler: (Result<ValueType> -> Void)?) {
+		guard let handler = handler else {
+			log("'\(message())': \(error)", function: function, file: file, line: line)
+			return
+		}
+
+		handler(.Failure(error))
+	}
+}
