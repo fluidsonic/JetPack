d6d3b6b576c4ab1523e65215fe67dab348850641
Marc Knaup <marc@knaup.koeln>
2015-11-27 18:51:22 +0100

    Added NSURL.URLByAppendingQueryItems(_:)

--- Sources/extensions/Foundation/NSURL.swift
+++ Sources/extensions/Foundation/NSURL.swift
@@ -13,4 +13,19 @@ public extension NSURL {
 	public convenience init(forPhoneCallPromptWithNumber phoneNumber: String) {
 		self.init(string: "telprompt:\(phoneNumber.urlEncodedPath)")!
 	}
+
+
+	public func URLByAppendingQueryItems(queryItems: [NSURLQueryItem]) -> NSURL? {
+		guard !queryItems.isEmpty else {
+			return self
+		}
+
+		guard let urlComponents = NSURLComponents(URL: self, resolvingAgainstBaseURL: false) else {
+			return nil
+		}
+
+		urlComponents.queryItems = (urlComponents.queryItems ?? []) + queryItems
+
+		return urlComponents.URL
+	}
 }
