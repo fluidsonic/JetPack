76f05d9c9439d13bfeb26c93533ac1aac7a473de
Marc Knaup <marc@knaup.koeln>
2015-11-28 14:45:18 +0100

    Fixed broken Int32.random(start:end:)

--- Sources/extensions/Swift/Int32.swift
+++ Sources/extensions/Swift/Int32.swift
@@ -5,7 +5,6 @@ extension Int32: RandomizableIntegerType {
 
 	@warn_unused_result
 	public static func random(start start: Int32, end: Int32) -> Int32 {
-		let r = arc4random_uniform(UInt32(Int64(start) - Int64(end)))
-		return Int32(Int64(r) + Int64(end))
+		return Int32(arc4random_uniform(UInt32(end - start))) + start
 	}
 }
