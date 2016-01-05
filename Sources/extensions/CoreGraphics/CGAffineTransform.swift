4db8eb12529e3ca8ace892aa8beb6853e37c8ba5
Marc Knaup <marc@knaup.koeln>
2016-01-05 15:16:15 +0100

    Improved CGAffineTransform extension

--- Sources/extensions/CoreGraphics/CGAffineTransform.swift
+++ Sources/extensions/CoreGraphics/CGAffineTransform.swift
@@ -36,6 +36,48 @@ public extension CGAffineTransform {
 	}
 
 
+	public mutating func concatenateWith(transform: CGAffineTransform) {
+		self = concatenatedWith(transform)
+	}
+
+
+	@warn_unused_result(mutable_variant="concatenateWith")
+	public func concatenatedWith(transform: CGAffineTransform) -> CGAffineTransform {
+		return CGAffineTransformConcat(self, transform)
+	}
+
+
+	public var horizontalScale: CGFloat {
+		return sqrt((a * a) + (c * c))
+	}
+
+
+	public var horizontalTranslation: CGFloat {
+		return tx
+	}
+
+
+	public mutating func inverse() {
+		self = inverted()
+	}
+
+
+	@warn_unused_result(mutable_variant="invert")
+	public func inverted() -> CGAffineTransform {
+		return CGAffineTransformInvert(self)
+	}
+
+
+	public var isIdentity: Bool {
+		return CGAffineTransformIsIdentity(self)
+	}
+
+
+	public var rotation: CGFloat {
+		return atan2(b, a)
+	}
+
+
 	public mutating func rotateBy(angle: CGFloat) {
 		self = rotatedBy(angle)
 	}
@@ -99,6 +141,71 @@ public extension CGAffineTransform {
 	@warn_unused_result(mutable_variant="translateBy")
 	public func translatedBy(vertically vertical: CGFloat) -> CGAffineTransform {
 		return translatedBy(horizontally: 0, vertically: vertical)
+	}
+
+
+	public var verticalScale: CGFloat {
+		return sqrt((b * b) + (d * d))
+	}
+
+
+	public var verticalTranslation: CGFloat {
+		return ty
+	}
+}
+
+
+extension CGAffineTransform: CustomStringConvertible {
+
+	public var description: String {
+		guard !isIdentity else {
+			return "CGAffineTransform.identity"
+		}
+
+		let horizontalScale = self.horizontalScale
+		let verticalScale = self.verticalScale
+		let horizontalTranslation = self.horizontalTranslation
+		let verticalTranslation = self.verticalTranslation
+		let rotation = self.rotation
+
+		var description = ""
+
+		if horizontalScale != 1 {
+			if !description.isEmpty { description += ", " }
+			description += "horizontalScale: "
+			description += String(horizontalScale)
+		}
+
+		if verticalScale != 1 {
+			if !description.isEmpty { description += ", " }
+			description += "verticalScale: "
+			description += String(verticalScale)
+		}
+
+		if rotation != 0 {
+			if !description.isEmpty { description += ", " }
+			description += "rotation: "
+			description += String(rotation / .Pi)
+			description += " * .Pi"
+		}
+
+		if horizontalTranslation != 0 {
+			if !description.isEmpty { description += ", " }
+			description += "horizontalTranslation: "
+			description += String(horizontalTranslation)
+		}
+
+		if verticalTranslation != 0 {
+			if !description.isEmpty { description += ", " }
+			description += "verticalTranslation: "
+			description += String(verticalTranslation)
+		}
+
+		guard !description.isEmpty else {
+			return "CGAffineTransform.identity"
+		}
+
+		return "CGAffineTransform(" + description + ")"
 	}
 }
 
