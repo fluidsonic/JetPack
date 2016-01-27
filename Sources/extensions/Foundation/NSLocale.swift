e4c0bdfc2c67bddab0446187ea725ae2a850d4c8
Marc Knaup <marc@knaup.koeln>
2016-01-27 16:32:43 +0100

    Added NSLocale.englishUnitedStatesComputer

--- Sources/extensions/Foundation/NSLocale.swift
+++ Sources/extensions/Foundation/NSLocale.swift
@@ -4,6 +4,10 @@ import Foundation
 public extension NSLocale {
 
 	@nonobjc
+	public static let englishUnitedStatesComputer = NSLocale(localeIdentifier: "en_US_POSIX")
+
+
+	@nonobjc
 	public var uses24HourFormat: Bool {
 		return !(NSDateFormatter.dateFormatFromTemplate("j", options: 0, locale: self)?.containsString("a") ?? false)
 	}
