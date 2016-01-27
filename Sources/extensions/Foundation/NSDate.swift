84a70e9de791d471893bb96eea95828c0df5f50e
Marc Knaup <marc@knaup.koeln>
2016-01-27 16:33:12 +0100

    Added NSDate.iso8859

--- Sources/extensions/Foundation/NSDate.swift
+++ Sources/extensions/Foundation/NSDate.swift
@@ -1,6 +1,15 @@
 import Foundation
 
 
+public extension NSDate {
+
+	@nonobjc
+	public var iso8859: String {
+		return NSDateFormatter.iso8859Formatter.stringFromDate(self)
+	}
+}
+
+
 extension NSDate: Comparable {}
 
 public func < (a: NSDate, b: NSDate) -> Bool {
