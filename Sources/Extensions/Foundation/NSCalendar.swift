02b11a2c497f1da94baa7fba194d1565f3fa78e7
Marc Knaup <marc@knaup.koeln>
2016-04-12 17:48:34 +0200

    Added NSCalendar.firstDateOfWeek

--- Sources/Extensions/Foundation/NSCalendar.swift
+++ Sources/Extensions/Foundation/NSCalendar.swift
@@ -33,6 +33,15 @@ public extension NSCalendar {
 
 	@nonobjc
 	@warn_unused_result
+	public func firstDateOfWeek(date: NSDate) -> NSDate? {
+		var firstDateOfWeek: NSDate?
+		rangeOfUnit(.WeekOfYear, startDate: &firstDateOfWeek, interval: nil, forDate: date)
+		return firstDateOfWeek
+	}
+
+
+	@nonobjc
+	@warn_unused_result
 	public func dateBySubtracting(seconds seconds: Int? = nil, minutes: Int? = nil, hours: Int? = nil, days: Int? = nil, months: Int? = nil, years: Int? = nil, fromDate date: NSDate) -> NSDate? {
 		return dateByAdding(seconds: seconds.map(-), minutes: minutes.map(-), hours: hours.map(-), days: days.map(-), months: months.map(-), years: years.map(-), toDate: date)
 	}
