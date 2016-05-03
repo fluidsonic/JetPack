d61dcfbecdd178642eabf483e8995fed8b85306e
Marc Knaup <marc@knaup.koeln>
2016-05-03 17:25:53 +0200

    Added NSMutableAttributedString.appendString(_:paragraphStyle:)

--- Sources/Extensions/UIKit/NSMutableAttributedString+UIKit.swift
+++ Sources/Extensions/UIKit/NSMutableAttributedString+UIKit.swift
@@ -6,9 +6,10 @@ public extension NSMutableAttributedString {
 	@nonobjc
 	private func _appendString(string: String,
 	                           maintainingPrecedingAttributes: Bool = false,
-	                           font: UIFont? = nil,
-	                           foregroundColor: UIColor? = nil,
-	                           link: NSURL? = nil)
+	                           font: UIFont?,
+	                           foregroundColor: UIColor?,
+	                           link: NSURL?,
+	                           paragraphStyle: NSParagraphStyle?)
 	{
 		var attributes = [String : AnyObject]()
 		if let font = font {
@@ -20,6 +21,9 @@ public extension NSMutableAttributedString {
 		if let link = link {
 			attributes[NSLinkAttributeName] = link
 		}
+		if let paragraphStyle = paragraphStyle {
+			attributes[NSParagraphStyleAttributeName] = paragraphStyle
+		}
 
 		appendString(string, maintainingPrecedingAttributes: maintainingPrecedingAttributes, additionalAttributes: attributes)
 	}
@@ -30,9 +34,10 @@ public extension NSMutableAttributedString {
 	                         maintainingPrecedingAttributes: Bool = false,
 	                         font: UIFont?,
 	                         foregroundColor: UIColor? = nil,
-	                         link: NSURL? = nil)
+	                         link: NSURL? = nil,
+	                         paragraphStyle: NSParagraphStyle? = nil)
 	{
-		_appendString(string, maintainingPrecedingAttributes: maintainingPrecedingAttributes, font: font, foregroundColor: foregroundColor, link: link)
+		_appendString(string, maintainingPrecedingAttributes: maintainingPrecedingAttributes, font: font, foregroundColor: foregroundColor, link: link, paragraphStyle: paragraphStyle)
 	}
 
 
@@ -40,17 +45,28 @@ public extension NSMutableAttributedString {
 	public func appendString(string: String,
 	                         maintainingPrecedingAttributes: Bool = false,
 	                         foregroundColor: UIColor?,
-	                         link: NSURL? = nil)
+	                         link: NSURL? = nil,
+	                         paragraphStyle: NSParagraphStyle? = nil)
 	{
-		_appendString(string, maintainingPrecedingAttributes: maintainingPrecedingAttributes, font: nil, foregroundColor: foregroundColor, link: link)
+		_appendString(string, maintainingPrecedingAttributes: maintainingPrecedingAttributes, font: nil, foregroundColor: foregroundColor, link: link, paragraphStyle: paragraphStyle)
 	}
 
 
 	@nonobjc
 	public func appendString(string: String,
 	                         maintainingPrecedingAttributes: Bool = false,
-	                         link: NSURL?)
+	                         link: NSURL?,
+	                         paragraphStyle: NSParagraphStyle? = nil)
 	{
-		_appendString(string, maintainingPrecedingAttributes: maintainingPrecedingAttributes, font: nil, foregroundColor: nil, link: link)
+		_appendString(string, maintainingPrecedingAttributes: maintainingPrecedingAttributes, font: nil, foregroundColor: nil, link: link, paragraphStyle: paragraphStyle)
+	}
+
+
+	@nonobjc
+	public func appendString(string: String,
+	                         maintainingPrecedingAttributes: Bool = false,
+	                         paragraphStyle: NSParagraphStyle?)
+	{
+		_appendString(string, maintainingPrecedingAttributes: maintainingPrecedingAttributes, font: nil, foregroundColor: nil, link: nil, paragraphStyle: paragraphStyle)
 	}
 }
