c763ccec286a5cf3b29b1075fccc3906d7fdb7fa
Marc Knaup <marc@knaup.koeln>
2016-05-20 15:03:36 +0200

    Merged #12 with consistent code style

--- Sources/Extensions/Swift/SignedNumberType.swift
+++ Sources/Extensions/Swift/SignedNumberType.swift
@@ -1,11 +1,13 @@
 public extension SignedNumberType {
-  var sign: Self {
-    if self == 0 {
-      return 0
-    }
-    else if self < 0 {
-      return -1
-    }
-    return 1
-  }
+
+	var sign: Self {
+		if self == 0 {
+			return 0
+		}
+		if self < 0 {
+			return -1
+		}
+
+		return 1
+	}
 }
