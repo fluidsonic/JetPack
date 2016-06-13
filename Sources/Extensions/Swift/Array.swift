3e138451e35685fe278ba19500a10d9ede9dd341
Marc Knaup <marc@knaup.koeln>
2016-06-13 17:28:59 +0200

    Added Array.getOrNil(_:)

--- Sources/Extensions/Swift/Array.swift
+++ Sources/Extensions/Swift/Array.swift
@@ -1,6 +1,16 @@
 public extension Array {
 
 	@warn_unused_result
+	public func getOrNil(index: Index) -> Generator.Element? {
+		guard indices.contains(index) else {
+			return nil
+		}
+
+		return self[index]
+	}
+
+
+	@warn_unused_result
 	public func toArray() -> [Generator.Element] {
 		return self
 	}
