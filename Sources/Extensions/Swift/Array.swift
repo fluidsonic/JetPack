2fe90f475366d30fb01139e73ba1b8efefd95a23
Marc Knaup <marc@knaup.koeln>
2016-12-15 17:24:43 +0100

    Added === and !== for Array

--- Sources/Extensions/Swift/Array.swift
+++ Sources/Extensions/Swift/Array.swift
@@ -15,3 +15,23 @@ public extension Array {
 		return self
 	}
 }
+
+
+public func === <T: AnyObject> (a: [T], b: [T]) -> Bool {
+	guard a.count == b.count else {
+		return false
+	}
+
+	for index in a.indices {
+		guard a[index] === b[index] else {
+			return false
+		}
+	}
+
+	return true
+}
+
+
+public func !== <T: AnyObject> (a: [T], b: [T]) -> Bool {
+	return !(a === b)
+}
