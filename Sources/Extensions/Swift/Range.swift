4e7661674aee12720685a1786738ff29bce02937
Marc Knaup <marc@knaup.koeln>
2017-02-10 18:41:15 +0100

    Added Range.mapNotNil

--- Sources/Extensions/Swift/Range.swift
+++ Sources/Extensions/Swift/Range.swift
@@ -1,3 +1,19 @@
+public extension Range {
+
+	public func mapNotNil <T: Comparable>(_ transform: (Bound) -> T?) -> Range<T>? {
+		guard
+			let lowerBound = transform(lowerBound),
+			let upperBound = transform(upperBound),
+			lowerBound < upperBound
+		else {
+				return nil
+		}
+
+		return lowerBound ..< upperBound
+	}
+}
+
+
 public extension Range where Bound: RandomizableIntegerType {
 
 	public var randomIndex: Bound? {
