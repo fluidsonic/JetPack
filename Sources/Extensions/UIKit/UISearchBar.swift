c8d8c9604324a14188cf6d66b7343a1bfd800f47
Marc Knaup <marc@knaup.koeln>
2017-05-04 15:16:49 +0200

    Added missing dynamic keyword

--- Sources/Extensions/UIKit/UISearchBar.swift
+++ Sources/Extensions/UIKit/UISearchBar.swift
@@ -4,7 +4,7 @@ import UIKit
 public extension UISearchBar {
 
 	@objc(JetPack_setAutomaticallyDisablesCancelButton:)
-	internal func private_setAutomaticallyDisablesCancelButton(_ automaticallyDisablesCancelButton: Bool) {
+	dynamic func private_setAutomaticallyDisablesCancelButton(_ automaticallyDisablesCancelButton: Bool) {
 		// UIKit automatically disables the cancel button unless the search bar is first responder.
 		// Going through the subviews to find the cancel button is more fragile than disabling it using a private method.
 		// If the private method no longer exists or changes signature the fix will simply stop working without harming the app.
@@ -12,7 +12,7 @@ public extension UISearchBar {
 
 
 	@nonobjc
-	internal static func UISearchBar_setUp() {
+	static func UISearchBar_setUp() {
 		redirectMethod(in: self, from: #selector(private_setAutomaticallyDisablesCancelButton(_:)), to: obfuscatedSelector("_", "set", "Auto", "Disable", "Cancel", "Button:"))
 	}
 }
