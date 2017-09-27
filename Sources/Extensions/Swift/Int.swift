0b7c57fa3cb73a4d2810a52e2c4cc4ecc9bddf8b
Marc Knaup <marc@knaup.koeln>
2017-09-28 01:51:19 +0200

    using __WORDSIZE is compile-time checked without warnings again! thanks @CodaFi

--- Sources/Extensions/Swift/Int.swift
+++ Sources/Extensions/Swift/Int.swift
@@ -4,10 +4,14 @@ import Darwin
 extension Int: RandomizableIntegerType {
 
 	public static func random(start: Int, end: Int) -> Int {
-		switch __WORDSIZE {
-		case 32: return Int(Int32.random(start: Int32(start), end: Int32(end)))
-		case 64: return Int(Int64.random(start: Int64(start), end: Int64(end)))
-		default: preconditionFailure("Unsupported architecture") // TODO would be great if we can catch this at compile-time
+		#if arch(arm) || arch(i386)
+		if __WORDSIZE == 32 {
+			return Int(Int32.random(start: Int32(start), end: Int32(end)))
 		}
+		#elseif arch(arm64) || arch(x86_64)
+		if __WORDSIZE == 64 {
+			return Int(Int64.random(start: Int64(start), end: Int64(end)))
+		}
+		#endif
 	}
 }
--- Sources/Extensions/Swift/UInt.swift
+++ Sources/Extensions/Swift/UInt.swift
@@ -4,10 +4,14 @@ import Darwin
 extension UInt: RandomizableIntegerType {
 
 	public static func random(start: UInt, end: UInt) -> UInt {
-		switch __WORDSIZE {
-		case 32: return UInt(UInt32.random(start: UInt32(start), end: UInt32(end)))
-		case 64: return UInt(UInt64.random(start: UInt64(start), end: UInt64(end)))
-		default: preconditionFailure("Unsupported architecture") // TODO would be great if we can catch this at compile-time
+		#if arch(arm) || arch(i386)
+		if __WORDSIZE == 32 {
+			return UInt(UInt32.random(start: UInt32(start), end: UInt32(end)))
 		}
+		#elseif arch(arm64) || arch(x86_64)
+		if __WORDSIZE == 64 {
+			return UInt(UInt64.random(start: UInt64(start), end: UInt64(end)))
+		}
+		#endif
 	}
 }
