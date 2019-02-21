52308223ebc01d679bdc20f237bd663bc12f4d33
Benjamin Werker <benjamin.werker@gmail.com>
2019-02-21 18:18:46 +0100

    updated NSDateFormatter extension to include option for fractional seconds

--- Sources/Extensions/Foundation/NSDateFormatter.swift
+++ Sources/Extensions/Foundation/NSDateFormatter.swift
@@ -3,11 +3,21 @@ import Foundation
 
 public extension DateFormatter {
 
+	@available(*, deprecated, message: "use 'iso8601Formatter(withFractionalSeconds: Bool)' instead")
 	@nonobjc
-	public static let iso8859Formatter: DateFormatter = {
-		let formatter = DateFormatter()
-		formatter.locale = Locale.englishUnitedStatesComputer
-		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-		return formatter
-	}()
+	public static let iso8859Formatter: DateFormatter = iso8601Formatter()
+
+	@nonobjc
+	public static func iso8601Formatter(withFractionalSeconds: Bool = false) -> DateFormatter {
+		let isoFormatter = DateFormatter()
+		isoFormatter.locale = Locale.englishUnitedStatesComputer
+		if withFractionalSeconds {
+			isoFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+		}
+		else {
+			isoFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+		}
+		
+		return isoFormatter
+	}
 }
