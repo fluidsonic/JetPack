947b56aba0846f391c628a74d04b45fd39774385
Benjamin Werker <benjamin.werker@gmail.com>
2019-02-25 10:11:16 +0100

    adjusted NSDateFormatter Extension for ISO8601 to use static fields instead of on demand creating new, setting timeZone according to Apple guideline too

--- Sources/Extensions/Foundation/NSDateFormatter.swift
+++ Sources/Extensions/Foundation/NSDateFormatter.swift
@@ -3,21 +3,37 @@ import Foundation
 
 public extension DateFormatter {
 
+	@nonobjc
+	private static let iso8601Formatter: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.locale = Locale.englishUnitedStatesComputer
+		formatter.timeZone = TimeZone(secondsFromGMT: 0)
+		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+
+		return formatter
+	}()
+
+	@nonobjc
+	private static let iso8601FormatterWithFractionalSeconds: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.locale = Locale.englishUnitedStatesComputer
+		formatter.timeZone = TimeZone(secondsFromGMT: 0)
+		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+
+		return formatter
+	}()
+
 	@available(*, deprecated, message: "use 'iso8601Formatter(withFractionalSeconds: Bool)' instead")
 	@nonobjc
-	public static let iso8859Formatter: DateFormatter = iso8601Formatter()
+	public static let iso8859Formatter: DateFormatter = iso8601Formatter
+
 
 	@nonobjc
 	public static func iso8601Formatter(withFractionalSeconds: Bool = false) -> DateFormatter {
-		let isoFormatter = DateFormatter()
-		isoFormatter.locale = Locale.englishUnitedStatesComputer
 		if withFractionalSeconds {
-			isoFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
-		}
-		else {
-			isoFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+			return iso8601FormatterWithFractionalSeconds
 		}
 		
-		return isoFormatter
+		return iso8601Formatter
 	}
 }
