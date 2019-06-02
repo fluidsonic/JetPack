a5bde2c4e1845aff96d5559d943a7a12f7b5b648
Marc Knaup <marc@knaup.io>
2019-06-02 02:40:24 +0100

    Removed unnecessary method

--- Sources/Extensions/Swift/Array.swift
+++ Sources/Extensions/Swift/Array.swift
@@ -15,14 +15,6 @@ public extension Array {
 }
 
 
-public extension Array where Iterator.Element: _Optional, Iterator.Element.Wrapped: Equatable {
-
-	func index(of element: Iterator.Element.Wrapped?) -> Int? {
-		return firstIndex { $0.value == element }
-	}
-}
-
-
 public func === <T: AnyObject> (a: [T], b: [T]) -> Bool {
 	guard a.count == b.count else {
 		return false
